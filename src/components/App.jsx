d5a601ad6cb3fcc58f70f76a34d1368a8051cd32
Liudmyla007 <ldatsenko3@gmail.com>
2023-04-23 23:07:10 +0300

    add ScreenSaver

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,3 +1,5 @@
+import { ScreenSaver } from "./ScreenSaver/ScreenSaver";
+
 export const App = () => {
   return (
     <div
@@ -10,7 +12,8 @@ export const App = () => {
         color: '#010101'
       }}
     >
-      React homework template
+      <ScreenSaver/>
+ 
     </div>
   );
 };
---
+++ src/components/ScreenSaver/ScreenSaver.styled.jsx
@@ -0,0 +1,8 @@
+import styled from 'styled-components';
+
+export const Img = styled.img`
+ border-radius: 15px;
+  box-shadow: 10px 10px 8px 2px rgba(0, 0, 0, 0.3);
+   flex-grow: 1;
+   flex: 1;
+`;
