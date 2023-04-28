c1ba00db43663828c77e243ed27d901c444651f7
Liudmyla007 <ldatsenko3@gmail.com>
2023-04-28 12:04:05 +0300

    add cursor pointer foe button

--- src/components/Button/Button.styled.jsx
+++ src/components/Button/Button.styled.jsx
@@ -10,11 +10,18 @@ export const Btn = styled.button`
   height: 50px;
   border: none;
   border-radius: 10px;
+  cursor: pointer;
   background-color: ${p => (p.selected ? '#5cd3a8' : '#ebd8ff')};
   color: #373737;
   font-weight: 500;
   font-size: 20px;
-  text-align: center;    
+  text-align: center;   
+  &:hover,
+  &:focus {
+    transform: scale(1.1);
+    border: 1px solid rgba(31, 10, 38, 1);
+    box-shadow: 0px 8px 8px 4px rgba(30, 46, 46, 1);
+  } 
 }  
 `;
 
